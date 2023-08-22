4edcbb436a18421ea0acdbd231a5c5dfa5646e78
JuniorAL930305 <a0079-junior@hotmail.com>
2023-08-21 19:16:06 -0500

    inicio proyecto sprint

---
+++ junior-eje1/src/test/java/com/junior/eje1/JuniorEje1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.eje1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorEje1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
